7e953cafb77208649d868071f7cfe5f812d1c707
yazid-hoblos <yazid.hoblos@lau.edu>
2025-10-22 23:07:14 +0200

    --interface optimization

--- biomera/api.py
+++ biomera/api.py
@@ -168,4 +168,4 @@ if __name__ == '__main__':
         print("Will initialize on first request instead.")
     
     print("Open http://localhost:5000 in your browser")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
